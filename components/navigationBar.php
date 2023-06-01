caef85e3a253966cee32dba80f3e354de11ba58d
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-01 13:18:52 +0800

    Update navigationBar.php

--- components/navigationBar.php
+++ components/navigationBar.php
@@ -10,22 +10,22 @@ $sessionuser = $_SESSION["username"] . " - " . $_SESSION["role"];
             <ul class="flex gap-4">
                 <li>Home</li>
                 <li>
-                    <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-white text-center inline-flex items-center" type="button">
+                    <button id="dropdownHoverButton" data-dropdown-toggle="dropdownReceive" data-dropdown-trigger="hover" class="text-white text-center inline-flex items-center" type="button">
                         Receive Delivery
                         <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                         </svg>
                     </button>
                     <!-- Dropdown menu -->
-                    <div id="dropdownHover" class="z-10 hidden bg-shade divide-y divide-gray-100 rounded-lg shadow w-44">
+                    <div id="dropdownReceive" class="z-10 hidden bg-shade divide-y divide-gray-100 rounded-lg shadow w-44">
                         <ul class="py-2 text-sm text-txt " aria-labelledby="dropdownHoverButton">
                             <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-accent hover:text-white">
+                                <a href="/manage/supplier/" class="block px-4 py-2 hover:bg-accent hover:text-white">
                                     Manage Suppliers
                                 </a>
                             </li>
                             <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-accent hover:text-white">
+                                <a href="/manage/product/" class="block px-4 py-2 hover:bg-accent hover:text-white">
                                     Manage Products
                                 </a>
                             </li>
@@ -38,11 +38,27 @@ $sessionuser = $_SESSION["username"] . " - " . $_SESSION["role"];
                     </div>
                 </li>
                 <li>
-                    Order Products
-                    <ul class="hidden">
-                        <li>Purchase Order Manage</li>
-                        <li>Expired Products Manage</li>
-                    </ul>
+                    <button id="dropdownHoverButton" data-dropdown-toggle="dropdownProduct" data-dropdown-trigger="hover" class="text-white text-center inline-flex items-center" type="button">
+                        Order Products
+                        <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg>
+                    </button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownProduct" class="z-10 hidden bg-shade divide-y divide-gray-100 rounded-lg shadow w-44">
+                        <ul class="py-2 text-sm text-txt " aria-labelledby="dropdownHoverButton">
+                            <li>
+                                <a href="#" class="block px-4 py-2 hover:bg-accent hover:text-white">
+                                    Purchase Order Manage
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="block px-4 py-2 hover:bg-accent hover:text-white">
+                                    Expired Products Manage
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </li>
                 <li>
                     <a class="dropdown-item" href="../login/logout.php">Log out</a>
