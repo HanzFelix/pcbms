5b222dc234f248402be864fedd3e0b4bc883c9e2
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-06-01 12:57:51 +0800

    Relocate supplier files

---
+++ manage/supplier/createSupplier.php
@@ -0,0 +1,16 @@
+<?php
+include "../../models/mgtDAO.php";
+
+$mgtdao = new MgtDAO();
+
+// Retrieve the submitted form data
+$name = $_POST['company-name'];
+$contact = $_POST['contact-person'];
+$sex = $_POST['sex'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+
+$supplier = $mgtdao->createSupplier($name, $contact, $sex, $address, $phone);
+
+// Return the response
+echo "success";
