8857546babf43ddd0023036ba2667888332a5408
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-08-18 13:20:28 +0800

    Add dashboard items

--- resources/views/manage/dashboard.php
+++ resources/views/manage/dashboard.php
@@ -7,11 +7,28 @@ if (isset($_SESSION['error_message'])) {
     unset($_SESSION['error_message']);
 }
 
+$urlList = [
+    ['url' => '/manage/supplier', 'title' => 'Manage Suppliers'],
+    ['url' => '/manage/product', 'title' => 'Manage Products'],
+    ['url' => '/manage/delivery', 'title' => 'Product Delivery'],
+    ['url' => '#', 'title' => 'Manage Purchase Order'],
+    ['url' => '#', 'title' => 'Manage Expired Products']
+];
+
 ob_start();
 ?>
 <!--Main-->
-<main class="p-4">
+<main class="p-8 container mx-auto">
     <h1 class="text-center text-3xl font-bold">Store Management</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-8">
+        <?php foreach ($urlList as $urlItem) : ?>
+            <a href="<?= $urlItem['url'] ?>">
+                <div class="flex justify-end flex-col border-l-4 border-accent/80 bg-accent/10 h-32 px-4 py-2">
+                    <span class="font-bold text-2xl text-accent"><?= $urlItem['title'] ?></span>
+                </div>
+            </a>
+        <?php endforeach; ?>
+    </div>
 </main>
 
 <?php
