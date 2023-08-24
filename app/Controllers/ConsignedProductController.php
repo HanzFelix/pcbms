35c74b94d49538ccf7cb2fd388c21506b2098fc5
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-08-24 20:26:37 +0800

    Tableify content

--- app/Controllers/ConsignedProductController.php
+++ app/Controllers/ConsignedProductController.php
@@ -2,22 +2,6 @@
 
 class ConsignedProductController
 {
-    public function searchConsignedDetails()
-    {
-        include $_SERVER['DOCUMENT_ROOT'] . "/app/Models/ConsignedDetailsModel.php";
-        $condetails = new ConsignedDetailsModel();
-
-        $result = $condetails->getConsignedDetailsList($_GET['search-input']);
-        // Build the HTML dropdown with the search results
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<button value="' . $row['cd_id'] . '" id="button-consigned_det" class="text-left truncate">' . $row['date'] . ' - ' . $row['company'] . '</button>';
-            }
-        } else {
-            echo 'No results found';
-        }
-    }
-
     public function getConsignedDetails()
     {
         include $_SERVER['DOCUMENT_ROOT'] . "/app/Models/ConsignedDetailsModel.php";
@@ -43,24 +27,25 @@ class ConsignedProductController
         include $_SERVER['DOCUMENT_ROOT'] . "/app/Models/ConsignedDetailsModel.php";
         $condetails = new ConsignedDetailsModel();
 
-        $result = $condetails->getConsignedDetailsListNew();
+        $result = $condetails->getConsignedDetailsList();
+        $data = [];
         if (mysqli_num_rows($result) > 0) {
-            $i = 0;
             while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr class="bg-accent ' . ($i % 2 == 0 ? 'bg-opacity-10' : 'bg-opacity-25') . '">
-                <td class="px-4 py-2">' . $row["cd_id"] . ' </td>
-                    <td class="px-4 py-2">' . $row["company"] . ' </td>
-                    <td class="px-4 py-2">' . $row["username"] . '</td>
-                    <td class="px-4 py-2">' . $row["date"] . '</td>
-                    <td class="px-4 py-2">
-                        <button class="bg-primary text-white px-3 rounded-full py-1 text-xs" value="' . $row["cd_id"] . '">VIEW</button>
-                    </td>
-                </tr>';
-                $i++;
+                $data[] = [
+                    $row["cd_id"],
+                    $row["company"],
+                    $row["username"],
+                    $row["date"],
+                    '<button class="bg-primary text-white px-3 rounded-full py-1 text-xs" value="' . $row["cd_id"] . '">VIEW</button>',
+                ];
             }
         } else {
-            echo '<tr class="bg-accent bg-opacity-10"><td class="px-4 py-2">No results found</td></tr>';
+            $data[] = ['No results found'];
         }
+
+        $headerLabels = ["ID", "Supplier", "Received by", "Date Delivered", "Action"];
+        include $_SERVER['DOCUMENT_ROOT'] . "/resources/partials/table.php";
+        echo generateTable($data, $headerLabels);
     }
 
     public function getConsignedProductList()
@@ -69,26 +54,24 @@ class ConsignedProductController
         $conproduct = new ConsignedProductModel();
 
         $result = $conproduct->getConsignedProductList($_GET['id']);
-        // Build the HTML dropdown with the search results
+        $data = [];
         if (mysqli_num_rows($result) > 0) {
-            $i = 0;
             while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr class="bg-accent ' . ($i % 2 == 0 ? 'bg-opacity-10' : 'bg-opacity-25') . '">
-                    <td class="px-4 py-2">' . $row["product-name"] . ' (x' . $row["quantity"] . ') </td>
-                    <td class="px-4 py-2">' . $row["barcode"] . '</td>
-                    <td class="px-4 py-2">' . $row["particulars"] . '</td>
-                    <td class="px-4 py-2">' . $row["exp-date"] . '</td>
-                    <td class="px-4 py-2">Php ' . $row["unit-price"] . '</td>
-                    <td class="px-4 py-2">Php ' . $row["selling-price"] . '</td>
-                    <td class="px-4 py-2">
-                        <button class="bg-primary text-white px-3 rounded-full py-1 text-xs" value="' . $row["cp-id"] . '">EDIT</button>
-                    </td>
-                </tr>';
-                $i++;
+                $data[] = [
+                    $row["product-name"] . ' (x' . $row["quantity"] . ')',
+                    $row["barcode"],
+                    $row["particulars"],
+                    $row["exp-date"], $row["unit-price"], $row["selling-price"],
+                    '<button class="bg-primary text-white px-3 rounded-full py-1 text-xs" value="' . $row["cp-id"] . '">EDIT</button>',
+                ];
             }
         } else {
-            echo '<tr class="bg-accent bg-opacity-10"><td class="px-4 py-2" colspan="7">No results found</td></tr>';
+            $data[] = ['No results found'];
         }
+
+        $headerLabels = ["Product Name (Qty)", "Barcode", "Particular", "Expiry Date", "Unit Price", "Selling Price", "Action"];
+        include $_SERVER['DOCUMENT_ROOT'] . "/resources/partials/table.php";
+        echo generateTable($data, $headerLabels);
     }
 
     public function createConsignedProduct()
---
+++ resources/partials/table.php
@@ -0,0 +1,28 @@
+<?php
+function generateTable($data, $headerLabels)
+{
+    $html = '<table class="text-left rounded-md overflow-hidden w-full">';
+
+    // Generate table header
+    $html .= '<thead class="bg-accent bg-opacity-75 text-white border-primary sticky">';
+    $html .= '<tr>';
+    foreach ($headerLabels as $label) {
+        $html .= '<th class="px-4 py-2">' . htmlspecialchars($label) . '</th>';
+    }
+    $html .= '</tr>';
+    $html .= '</thead>';
+    $html .= '<tbody>';
+    $i = 0;
+    foreach ($data as $row) {
+        $html .= '<tr class="bg-accent ' . ($i % 2 == 0 ? 'bg-opacity-10' : 'bg-opacity-25') . '">';
+        foreach ($row as $cell) {
+            $html .= '<td class="px-4 py-2">' . $cell . '</td>';
+        }
+        $html .= '</tr>';
+        $i++;
+    }
+    $html .= '</tbody>';
+    $html .= '</table>';
+
+    return $html;
+}
