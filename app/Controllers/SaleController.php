ba8ac5cef8f655065d5e36d0d48dfbee728a545f
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-08-28 20:26:58 +0800

    Add empty sales controller

---
+++ app/Controllers/SaleController.php
@@ -0,0 +1,40 @@
+<?
+
+class SalesController
+{
+    public function getSaleDetailsList()
+    {
+    }
+
+    public function getSaleDetails()
+    {
+    }
+
+    public function getSaleProduct()
+    {
+    }
+
+    public function createSaleProduct()
+    {
+    }
+
+    public function updateSaleProduct()
+    {
+    }
+
+    public function deleteSaleProduct()
+    {
+    }
+
+    public function createSaleDetails()
+    {
+    }
+
+    public function updateSaleDetails()
+    {
+    }
+
+    public function deleteSaleDetails()
+    {
+    }
+}
