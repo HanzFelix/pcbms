e2e0c2b71fcbd2e52b520a8631cdbd3beaaf0682
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2023-08-28 20:26:43 +0800

    Format query statement

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -5,9 +5,19 @@ class ProductModel extends ConnectionModel
 {
     function getProductList()
     {
-        $query = "SELECT p.*, COALESCE(SUM(cp.quantity), 0) AS total_quantity FROM product p LEFT JOIN
-        consigned_product cp ON p.prod_id = cp.prod_id GROUP BY
-    p.prod_id;";
+        $query = "SELECT
+            p.*, 
+            COALESCE(SUM(cp.quantity), 0) - COALESCE(SUM(ep.quantity), 0) - COALESCE(SUM(sp.qty_sold), 0) AS total_quantity
+        FROM 
+            product p 
+        LEFT JOIN
+            consigned_product cp ON p.prod_id = cp.prod_id 
+        LEFT JOIN
+            expired_product ep ON ep.cp_id = cp.cp_id
+        LEFT JOIN
+            sale_product sp ON sp.cp_id = cp.cp_id 
+        GROUP BY
+            p.prod_id;";
         try {
             $this->openConnection();
 
